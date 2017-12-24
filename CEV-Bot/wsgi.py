f5249cc3e26745c4b8e9db63b0ebe663c54c23f8
abhi981996 <abhishektiwari981996@gmail.com>
2017-12-25 02:13:55 +0530

    changes done

--- CEV-Bot/wsgi.py
+++ CEV-Bot/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CEVBLOGCOMPETITION.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CEV-Bot.settings")
 
 application = get_wsgi_application()
